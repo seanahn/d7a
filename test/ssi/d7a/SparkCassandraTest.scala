c0bf1e94c01669d4a59bd6615cbf71266edf0162
Sean Ahn <seanahn@youriris.com>
2014-11-06 10:09:15 -0800

    commit classpath

--- test/ssi/d7a/SparkCassandraTest.scala
+++ test/ssi/d7a/SparkCassandraTest.scala
@@ -1,6 +1,7 @@
 package ssi.d7a
 
 import org.junit.Test
+import org.junit.Assert._
 import com.datastax.driver.core.Cluster
 import org.junit.Before
 import org.junit.After
@@ -33,20 +34,13 @@ class SparkCassandraTest {
         $("""CREATE TABLE test.kv(key text PRIMARY KEY, value int);""")
         $("""INSERT INTO test.kv(key, value) VALUES ('key1', 1)""")
         $("""INSERT INTO test.kv(key, value) VALUES ('key2', 2)""")
-//$("CREATE KEYSPACE test WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};")
-//        $("""CREATE KEYSPACE test WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'Analytics' : 1};""")
-        $("""CREATE TABLE test.words (word text PRIMARY KEY, count int);""")
-        $("""INSERT INTO test.words (word, count) VALUES ('foo', 10);""")
-        $("""INSERT INTO test.words (word, count) VALUES ('bar', 20);""")
         
         val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1").set("spark.cassandra.connection.rpc.port", "9160")
-//        val conf = new SparkConf(true).set("cassandra.connection.host", "127.0.0.1")
         val sc = new SparkContext("local", "test", conf)
-//        sc.addJar("/opt/play-2.2.1/repository/cache/com.datastax.spark/spark-cassandra-connector_2.10/jars/spark-cassandra-connector_2.10-1.1.0-beta1.jar")
-//        val csc = com.datastax.spark.connector.toSparkContextFunctions(sc)
         import com.datastax.spark.connector._
         val rdd = sc.cassandraTable("test", "kv")
 		println(rdd.count)
+		assertEquals(2, rdd.count)
 		println(rdd.first)
 
 		// exercise sql now
@@ -54,6 +48,12 @@ class SparkCassandraTest {
 		val csc: CassandraSQLContext = new CassandraSQLContext(sc)
 		csc.setKeyspace("tets")
         val srdd: SchemaRDD = csc.sql("select * from test.kv") // is this generating the full output
-        println ("count : " +  srdd.count) // does not work
+        println("count : " +  srdd.count)
+		assertEquals(2, rdd.count)
+        
+        val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
+        collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))
+		println(rdd.count)
+		assertEquals(4, rdd.count)
     }
 }
