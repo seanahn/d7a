ddcb6cdde71b9475ff937098d982853aaa609f08
Loren Bland <lbland@servicesource.com>
2014-11-10 16:46:36 -0800

    moved DataStax and Spark variables into an object so they can be serialized

--- test/ssi/d7a/InsertAssetTest.scala
+++ test/ssi/d7a/InsertAssetTest.scala
@@ -10,11 +10,19 @@ import org.apache.spark.SparkConf
 import scala.util.control.NonFatal
 import org.apache.spark.sql.cassandra.CassandraSQLContext
 
-class InsertAssetTest {
+object Spark {
     val cluster = Cluster.builder.addContactPoint("127.0.0.1").build
     val session = cluster.connect
-    
-    def $(query: String) = session.execute(query)
+    val path = s"${System.getProperty("user.dir")}/testResources/assets/assets.csv"
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1").set("spark.cassandra.connection.rpc.port", "9160")
+    val sc = new SparkContext("local", "test", conf)
+    val csc: CassandraSQLContext = new CassandraSQLContext(sc)
+    val logData = sc.textFile(path).map(line => line.split("\t"))
+}
+
+class InsertAssetTest extends java.io.Serializable {
+
+    def $(query: String) = Spark.session.execute(query)
     
     @Before
     def dropSchema {
@@ -24,8 +32,8 @@ class InsertAssetTest {
     @After
     def closeConnection {
 //        dropSchema
-        session.close
-        cluster.close
+        Spark.session.close
+        Spark.cluster.close
     }
 
     
@@ -35,29 +43,16 @@ class InsertAssetTest {
         import org.apache.spark.sql._
         $("""CREATE KEYSPACE testload WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };""")
         println("created key space")
-
         $("CREATE TABLE testload.assets(name text,state text, supportType text,someString text,someNumber text PRIMARY KEY,amount text,amountType text,startDate text,endDate text,quarter text);")
-
-        val path = s"${System.getProperty("user.dir")}/testResources/assets/assets.csv"
-        val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1").set("spark.cassandra.connection.rpc.port", "9160")
-        val sc = new SparkContext("local", "test", conf)
-        val logData = sc.textFile(path).map(line => line.split("\t"))
-        val rdd = sc.cassandraTable("testload", "assets");
         println("inserting data")
 
-        logData.foreach(line => 
-          println("INSERT INTO testload.assets(name,state,supportType,someString,someNumber,amount,amountType,startDate,endDate,quarter) "+" VALUES ('" +line(0) +"','" +line(1)+ "','"+line(2)+"','"+line(3)+"','"+line(4)+"','"+line(5)+"','"+line(6)+"','"+line(7)+"','"+line(8)+"','"+line(9)+"');") 
-       //(sc.parallelize(Array(line(0), line(1), line(2), line(3),line(4),line(5),line(6),line(7),line(8),line(9)))).saveToCassandra("testload", "assets", rdd.columnNames) 
-        )
 
-
-        println("done inserting")
-        val csc: CassandraSQLContext = new CassandraSQLContext(sc)
-        csc.setKeyspace("testload")
-
-        val srdd: SchemaRDD = csc.sql("select * from assets") // is this generating the full output
-        println("count : " +  srdd.count())
-        println(rdd.count)
-
+        for (line <- Spark.logData) {
+            $("INSERT INTO testload.assets(name,state,supportType,someString,someNumber,amount,amountType,startDate,endDate,quarter) "+" VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');".format(line(0),line(1),line(2),line(3),line(4),line(5),line(6),line(7),line(8),line(9)))
+        }
+        
+        val srdd: SchemaRDD = Spark.csc.sql("select * from testload.assets") // is this generating the full output
+        println("count : " +  srdd.count)
+        println("Done")
     }
 }
